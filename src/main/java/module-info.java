bc7048836f9e5aa7f7ffb307f5db1c5b1b6f5af1
mikel <mikel.ra.2003@gmail.com>
2025-02-07 09:17:24 +0100

    add:module info cambiado

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,13 @@
 module org.mikel.examen_dein_2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
+    requires java.sql;
+    requires net.sf.jasperreports.core;
+    requires java.desktop;
 
-
+    opens org.mikel.dein_proyecto_2.controladores to javafx.fxml;
+    opens org.mikel.dein_proyecto_2.modelos to javafx.base;
     opens org.mikel.examen_dein_2 to javafx.fxml;
     exports org.mikel.examen_dein_2;
 }
